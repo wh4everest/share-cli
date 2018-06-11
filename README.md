698601d45948bf46a2bb58107664f8111a2d66bf
Andrej T <andrejt@hfour.com>
2018-06-11 22:04:13 +0200

    uploading and downloading works

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ sudo apt-add-repository http://share-cli-ubuntu.andrejt.com/
 sudo apt-get update
 sudo apt-get install share
 ```
+
+## Running the server
+
+```
+cd server/
+pip install flask
+export FLASK_APP=server.py
+python -m flask run
+```
--- server/server.py
+++ server/server.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import os
+import string
+import random
+
+from flask import Flask, request, send_from_directory
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
-app.debug = True
+app.config['UPLOAD_FOLDER'] = '/tmp/share-cli-uploads'
+
+def gen_safe_filename(filename):
+    chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    prefix = ''.join(random.SystemRandom().choice(chars) for _ in xrange(10))
+    filename = secure_filename(filename)
+    return prefix + '-' + filename
 
 @app.route("/", methods=['POST'])
-def hello():
-    return "Hello World!"
+def upload():
+    file = request.files['file']
+    filename = gen_safe_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return "http://localhost:5000/" + filename + '\n'
+
+@app.route("/<filename>")
+def download(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
